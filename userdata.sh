52a0eb87f213de87f9adff7495fe7c2213bf9583
vivekcn2 <vivekcn2@illinois.edu>
2021-07-17 10:07:47 -0500

    aws test

---
+++ userdata.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install nodejs
+sudo apt install npm
+sudo git clone https://github.com/VCNinc/pki-anonymization-experiment
+cd pki-anonymization-experiment
+sudo npm install
+sudo npm run amazon
