7e302e120e44d952ec1bc3f3e37772627c4687ea
vivekcn2 <vivekcn2@illinois.edu>
2021-07-16 20:14:50 -0500

    test

--- amazon.js
+++ amazon.js
@@ -1,5 +1,5 @@
 const axios = require('axios')
-const node = require('node')
+const node = require('./node/node')
 
 axios.get('http://169.254.169.254/latest/meta-data/local-ipv4').then((data) => {
   console.log(data)
