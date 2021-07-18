a35012182a937a42957fd18b49a8e659b8b8b061
vivekcn2 <vivekcn2@illinois.edu>
2021-07-18 01:54:38 -0500

    aws test results part 1

--- remote/remote.js
+++ remote/remote.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 const fs = require('fs')
 
-const core = 'http://18.218.248.166:3000/'  // remote
+const core = 'http://3.135.239.252:3000/'  // remote
 // const core = 'http://localhost:3000/' // local
 
 function connect () {
@@ -80,7 +80,6 @@ async function run (name, total, target = 32, save = false) {
 }
 
 (async () => {
-  await run('single-chat-room-trs', 2, 32, true)
-  await run('single-chat-room-trs', 4, 32, true)
-  await run('single-chat-room-trs', 8, 32, true)
+  await run('single-chat-room-pkia', 1024, 32, true)
+  // await run('single-chat-room-trs', 512, 32, true)
 })()
